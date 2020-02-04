d120c696174a6061a32f67b150501fb487a1e01b
andres107 <andres.ramirez@vincoorbis.com>
2020-02-04 17:26:18 -0600

    component: use components for cicle life button and change background

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { randomColor } from "./helpers/nice"
 import HelloWorldClass from "./components/HelloWorldClass";
 import HelloWorldFunction from "./components/HelloWorldFunction";
+import CicleLifeButton from "./components/CicleLifeButton";
 
 function App() {
+  const [showButton, setShowButton] = useState(true);
+  const [background, setBackground] = useState("#282c34");
+
+
+  const handleBackground = () => {
+    setBackground(randomColor());
+  };
+
+  const handleClickCicleLife = () => {
+    setShowButton(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header" style={{backgroundColor: background}}>
+        <img src={logo} className="App-logo" alt="logo"/>
         <HelloWorldClass text="react"/>
         <HelloWorldFunction text="react"/>
+        { showButton && <CicleLifeButton
+            color="#2C9CB2"
+            text="1010101010101"
+            onClick={handleClickCicleLife}
+          />
+        }
+        <br/>
+        <br/>
+        <button onClick={handleBackground}>Change background</button>
       </header>
     </div>
   );
