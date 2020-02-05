9b84dc418212e3dd732f7399f4204b05c73fd839
andres107 <andres.ramirez@vincoorbis.com>
2020-02-05 06:30:14 -0600

    comp: add user card component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { randomColor } from "./helpers/nice"
+import { createRandomUsers } from "./helpers/faker"
 import HelloWorldClass from "./components/HelloWorldClass";
 import HelloWorldFunction from "./components/HelloWorldFunction";
 import CicleLifeButton from "./components/CicleLifeButton";
+import UserCard from "./components/userCard";
 
 function App() {
   const [showButton, setShowButton] = useState(true);
   const [background, setBackground] = useState("#282c34");
-
+  const users = createRandomUsers(20);
 
   const handleBackground = () => {
     setBackground(randomColor());
@@ -34,6 +36,21 @@ function App() {
         <br/>
         <br/>
         <button onClick={handleBackground}>Change background</button>
+        <br/>
+        <br/>
+        <table>
+          <thead>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              return (
+                <tr key={user.name + user.lastName}>
+                  <td><UserCard {...user} /></td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </header>
     </div>
   );
